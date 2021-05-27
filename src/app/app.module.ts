596bd428b3e6cdbf7ebf75e425d62d9657c8eb2f
Devadeep Deb <devadeep.deb@gmail.com>
2021-05-27 20:10:55 +0530

    committed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,38 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { LoginService } from './login.service';
+import { routing } from './app.routing';
+import { UserAccountComponent } from './user-account/user-account.component';
+import { UserService } from './user.service';
+import { PrimaryTransactionComponent } from './primary-transaction/primary-transaction.component';
+import { SavingsTransactionComponent } from './savings-transaction/savings-transaction.component';
+import { RequestComponent } from './request/request.component';
+import { RequestService } from './request.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    UserAccountComponent,
+    PrimaryTransactionComponent,
+    SavingsTransactionComponent,
+    RequestComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    LoginService,
+    UserService,
+    RequestService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
